84667900eeb8c4b2d4de5108d4fa0af00250c9ed
Rubén de Celis Hernández <contact@rdch106.hol.es>
2019-01-21 01:37:24 +0100

    Add reporthook

--- http_downloader.py
+++ http_downloader.py
@@ -1,8 +1,21 @@
+import sys
 import urllib.request
+
+def reporthook(blocknum, blocksize, totalsize):
+    readsofar = blocknum * blocksize
+    if totalsize > 0:
+        percent = readsofar * 1e2 / totalsize
+        s = "\r%5.1f%% %*d / %d" % (
+            percent, len(str(totalsize)), readsofar, totalsize)
+        sys.stderr.write(s)
+        if readsofar >= totalsize: # near the end
+            sys.stderr.write("\n")
+    else: # total size is unknown
+        sys.stderr.write("read %d\n" % (readsofar,))
 
 url = "https://raw.githubusercontent.com/RDCH106/i-love-firefox/183266a9/I_Love_Firefox_220x56.png"
 print ("download start!")
-filename, headers = urllib.request.urlretrieve(url, filename="I_Love_Firefox_220x56.png")
+filename, headers = urllib.request.urlretrieve(url, filename="I_Love_Firefox_220x56.png", reporthook=reporthook)
 print ("download complete!")
 print ("download file location: ", filename)
 print ("download headers: ", headers)
