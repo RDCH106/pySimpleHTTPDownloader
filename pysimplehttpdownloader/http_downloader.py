759601d4f6af2a50adac85dc5119edbe0657ed7b
rdecelis <rdecelis@stt-systems.com>
2019-01-22 17:20:55 +0100

    Refactorize to use POO

--- pysimplehttpdownloader/http_downloader.py
+++ pysimplehttpdownloader/http_downloader.py
@@ -1,21 +1,38 @@
 import sys
 import urllib.request
 
-def reporthook(blocknum, blocksize, totalsize):
-    readsofar = blocknum * blocksize
-    if totalsize > 0:
-        percent = readsofar * 1e2 / totalsize
-        s = "\r%5.1f%% %*d / %d" % (
-            percent, len(str(totalsize)), readsofar, totalsize)
-        sys.stderr.write(s)
-        if readsofar >= totalsize: # near the end
-            sys.stderr.write("\n")
-    else: # total size is unknown
-        sys.stderr.write("read %d\n" % (readsofar,))
 
-url = "https://raw.githubusercontent.com/RDCH106/i-love-firefox/183266a9/I_Love_Firefox_220x56.png"
-print ("download start!")
-filename, headers = urllib.request.urlretrieve(url, filename="I_Love_Firefox_220x56.png", reporthook=reporthook)
-print ("download complete!")
-print ("download file location: ", filename)
-print ("download headers: ", headers)
+class HTTPDownloader(object):
+
+    def __init__(self, url=None):
+        self.__url = url
+
+    @staticmethod
+    def __reporthook__(blocknum, blocksize, totalsize):
+        readsofar = blocknum * blocksize
+        if totalsize > 0:
+            percent = readsofar * 1e2 / totalsize
+            s = "\r%5.1f%% %*d / %d" % (
+                percent, len(str(totalsize)), readsofar, totalsize)
+            sys.stderr.write(s)
+            if readsofar >= totalsize:  # near the end
+                sys.stderr.write("\n")
+        else:  # total size is unknown
+            sys.stderr.write("read %d\n" % (readsofar,))
+
+    def run(self):
+        print("download start!")
+        filename, headers = urllib.request.urlretrieve(
+            self.__url,
+            filename="I_Love_Firefox_220x56.png",
+            reporthook=self.__reporthook__)
+        print("download complete!")
+        print("download file location: ", filename)
+        print("download headers: ", headers)
+
+
+if __name__ == "__main__":
+    downloader = HTTPDownloader(
+        "https://raw.githubusercontent.com/RDCH106/i-love-firefox/183266a9/I_Love_Firefox_220x56.png"
+    )
+    downloader.run()
